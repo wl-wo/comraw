b70dd42eb2900517aae284309017c6fee5b40ce6
Stephen Hellings <stephen@hellings.cc>
2026-03-09 11:37:24 -0400

    H

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -19,20 +19,4 @@ declare global {
   }
 }
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    'iconify-icon': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        icon?: string;
-        width?: string | number;
-        height?: string | number;
-        inline?: boolean;
-        rotate?: string | number;
-        flip?: string;
-      },
-      HTMLElement
-    >;
-  }
-}
-
 export {};
